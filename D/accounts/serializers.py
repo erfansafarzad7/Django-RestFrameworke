f73aaf4723222d760cc90356df6c3785c459b32a
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-11-15 18:55:33 +0330

    validation

--- D/accounts/serializers.py
+++ D/accounts/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 
 
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError('admin cannot be in email')
+
 class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('useranme cant be admin')
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('password most match')
+        return data
+
+
 
