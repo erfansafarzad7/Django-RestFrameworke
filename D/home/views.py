a1594ab85e154cc6abc348a3c20274dc2c2cfcb6
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-11-15 16:52:38 +0330

    serializers

--- D/home/views.py
+++ D/home/views.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
 
 
 class Home(APIView):
     def get(self, request):
-        return Response({'name': 'jack'})
-
-    def post(self, request):
-        name = request.data['name']
-        return Response({'name': name})
+        persons = Person.objects.all()
+        ser_data = PersonSerializer(instance=persons, many=True)
+        return Response(data=ser_data.data)
 
