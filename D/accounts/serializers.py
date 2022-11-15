fe064db2134e68a46d39d64494a24f969efed7d7
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-11-15 19:47:25 +0330

    model-serializer

--- D/accounts/serializers.py
+++ D/accounts/serializers.py
@@ -1,25 +1,30 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 def clean_email(value):
     if 'admin' in value:
         raise serializers.ValidationError('admin cannot be in email')
 
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
+
+class UserRegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'validators': (clean_email,)}
+        }
 
     def validate_username(self, value):
         if value == 'admin':
-            raise serializers.ValidationError('useranme cant be admin')
+            raise serializers.ValidationError('username cant be admin')
         return value
 
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError('password most match')
         return data
-
-
 
