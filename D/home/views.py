a9bd4bec3df554af9a2b12e3de20054dd2e62992
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-11-17 19:17:10 +0330

    token-permissions

--- D/home/views.py
+++ D/home/views.py
@@ -2,9 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth.models import User
 
 
 class Home(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUser, ]
+
     def get(self, request):
         persons = Person.objects.all()
         ser_data = PersonSerializer(instance=persons, many=True)
