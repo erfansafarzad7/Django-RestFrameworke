b0df961dc9f863a0c2a41eae4d73d231c4f9ebce
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-11-17 18:44:28 +0330

    token-authentication

--- D/accounts/urls.py
+++ D/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as token_auth
 
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegister.as_view(), name='register'),
+    path('api-token-auth/', token_auth.obtain_auth_token),
 ]
--- D/accounts/views.py
+++ D/accounts/views.py
@@ -11,5 +11,5 @@ class UserRegister(APIView):
         ser_data = UserRegisterSerializer(data=request.POST)
         if ser_data.is_valid():
             ser_data.create(ser_data.validated_data)
-            return Response(ser_data.data, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
