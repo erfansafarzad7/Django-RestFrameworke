51c881e2100f9aae95520fb5da9fed610298b836
erfnasafazad7 <erfnasafazad7@gmail.com>
2022-12-03 19:30:17 +0330

    renderer&parser

--- D/D/settings.py
+++ D/D/settings.py
@@ -141,6 +141,16 @@ REST_FRAMEWORK = {
         'questions': '3/minute',
     },
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.AdminRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+
+    # 'DEFAULT_PARSER_CLASSES': [
+    #     'rest_framework.parsers.JSONParser',
+    # ],
 }
 
 SPECTACULAR_SETTINGS = {
